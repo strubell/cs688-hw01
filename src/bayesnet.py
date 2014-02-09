7e8ffc6e76665a7f52634ad3b7d11d3bc39f1e49
Emma Strubell <strubell@cs.umass.edu>
2014-02-09 18:02:09 -0500

    working except for big HD table

--- src/bayesnet.py
+++ src/bayesnet.py
@@ -26,27 +26,44 @@ def compute_cpts_from_dat(graph, domains, data_idx, data_fname):
         # read in counts from data (need to subtract 1 to get 0 indexing)
         for i in raw_data[:, indices]-1:
             cpt[tuple(i)] += 1.0
-                    
+        
+        print "counts:", cpt
+        rolled_cpt = np.rollaxis(cpt, axis=-1)
+        print "counts rolled:", rolled_cpt
+              
         # normalize to get cpt rather than counts
-        sums = np.sum(cpt, axis=-1)
+        #sums = np.sum(cpt, axis=0)
+        sums = np.sum(cpt, axis=0)
+        print "sums:", sums
         if sums.shape:
+            sums = np.transpose(sums)
+            print "sums tranpose:", sums
             for i in range(cpt.shape[-1]):
-                for j in range(len(cpt[...,i])):
-                    cpt[...,i][j] /= sums[j]
+                print "rows:", cpt[...,i], "sums:", sums[i]
+                print "transpose:", np.transpose(cpt[...,i])
+                print "sums:", np.transpose(sums[i,np.newaxis])
+                cpt[...,i] = np.transpose(np.transpose(cpt[...,i]) / np.transpose(sums[i,np.newaxis]))
+                print cpt[...,i]
+                #for j in range(len(cpt[...,i])):
+                    #print "row:", cpt[...,i][j], "sum:", sums[i]
+                    #cpt[...,i][j] /= sums[i]
         else:
             cpt = cpt/sums
         cpts[key] = cpt
+#         if sums.shape:
+#             for i in range(cpt.shape[-1]):
+#                 for j in range(len(cpt[...,i])):
+#                     cpt[...,i][j] /= sums[j]
+#         else:
+#             cpt = cpt/sums
+#         cpts[key] = cpt
     print
     return cpts
 
 def print_cpt(variable, cpts, graph, variable_map):
     cpt = cpts[variable]
     parents = graph[variable]
-    
     print "P(%s%s)" % (variable, "|"+",".join(parents) if parents else "")
-    
-    
-    
     print cpt
     print
     
